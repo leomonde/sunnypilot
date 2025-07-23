fe00af6cda94fc0bc88053b241ff817225f851ff
leomonde <gianlucafedele@gmail.com>
2025-07-23 10:32:12 -0300

    increase deadzone

--- opendbc_repo/opendbc/car/volvo/values.py
+++ opendbc_repo/opendbc/car/volvo/values.py
@@ -67,7 +67,7 @@ class CarControllerParams:
   # When close to desired steering angle, don't change steer direction inside deadzone.
   # Since we need to release control of the steering wheel for a brief moment, steering wheel will
   # unwind by itself.
-  DEADZONE = 0.2
+  DEADZONE = 5
 
   STEER_STEP = 2
   STEER_MAX = 500
