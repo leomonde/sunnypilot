e955e03a0f9e9e83f7574fff28d44d9605d1da5e
leomonde <129780808+leomonde@users.noreply.github.com>
2025-07-23 13:27:45 -0300

    Update values.py

--- opendbc_repo/opendbc/car/volvo/values.py
+++ opendbc_repo/opendbc/car/volvo/values.py
@@ -71,8 +71,8 @@ class CarControllerParams:
 
   STEER_STEP = 2
   STEER_MAX = 1000
-  STEER_DELTA_UP = 30    # torque increase per refresh 15 default
-  STEER_DELTA_DOWN = 20  # torque decrease per refresh 10 default
+  STEER_DELTA_UP = 10    # torque increase per refresh 15 default
+  STEER_DELTA_DOWN = 10  # torque decrease per refresh 10 default
   STEER_DRIVER_ALLOWANCE = 100  # allowed driver torque before start limiting
   STEER_DRIVER_MULTIPLIER = 2  # weight driver torque
   STEER_DRIVER_FACTOR = 100
