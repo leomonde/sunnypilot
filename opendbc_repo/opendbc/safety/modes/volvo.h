461c166c2a0e6af57f8dfd0ec97e42ffdd6ee952
leomonde <gianlucafedele@gmail.com>
2025-07-28 16:42:49 -0300

    Add intercept FSM0 and set 0 the ACC_FrontCar

--- opendbc_repo/opendbc/safety/modes/volvo.h
+++ opendbc_repo/opendbc/safety/modes/volvo.h
@@ -21,6 +21,7 @@
 static const CanMsg VOLVO_EUCD_TX_MSGS[] = {
     {VOLVO_EUCD_CCButtons, VOLVO_MAIN_BUS, 8, .check_relay = false},
     {VOLVO_EUCD_PSCM1,     VOLVO_CAM_BUS,  8, .check_relay = true},
+    {VOLVO_EUCD_FSM0,      VOLVO_MAIN_BUS, 8, .check_relay = true},
     {VOLVO_EUCD_FSM1,      VOLVO_MAIN_BUS, 8, .check_relay = true},
     {VOLVO_EUCD_FSM2,      VOLVO_MAIN_BUS, 8, .check_relay = true},
     {VOLVO_EUCD_FSM3,      VOLVO_MAIN_BUS, 8, .check_relay = true}
